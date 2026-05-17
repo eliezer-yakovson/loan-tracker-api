a4b0dcd4a167a6570fa98c0b1084b36f2cb423ec
Eliezer Yakovson <e3251105@gmail.com>
2026-05-17 15:07:18 +0300

    Add is_admin migration + run migrations on startup

---
+++ migrations/versions/0004_add_is_admin.py
@@ -0,0 +1,26 @@
+"""Add is_admin column to users
+
+Revision ID: 0004
+Revises: 0003
+Create Date: 2026-05-17
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+revision: str = "0004"
+down_revision: Union[str, None] = "0003"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "users",
+        sa.Column("is_admin", sa.Boolean(), nullable=False, server_default="false"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "is_admin")
